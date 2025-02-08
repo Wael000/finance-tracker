16a4e0b1fdcd3ad78b28744b35b40852b3d98e4f
Wael000 <58821483+Wael000@users.noreply.github.com>
2025-02-08 23:28:04 +0100

    t  Initial commit

---
+++ client/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+function App() {
+
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<h1>Dashbboard</h1>} />
+        <Route path="/auth" element={<h1>Sign in</h1>} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
